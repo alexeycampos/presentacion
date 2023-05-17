057ab9bbdd82135519ad9ba7570ab5e5028702a5
alexeycampos <alexeycampos31@gmail.com>
2023-05-16 20:21:02 -0500

    Se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DescripcionComponent } from './descripcion/descripcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescripcionComponent
   ],
   imports: [
     BrowserModule,
